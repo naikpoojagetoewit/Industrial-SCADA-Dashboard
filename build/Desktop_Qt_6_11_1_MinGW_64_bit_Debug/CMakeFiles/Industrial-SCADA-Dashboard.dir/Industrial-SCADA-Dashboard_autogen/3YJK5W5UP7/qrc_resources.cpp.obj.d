f488f09d781abd53592fd36715da5953fdbfd10e
naikpoojagetoewit <161451264+naikpoojagetoewit@users.noreply.github.com>
2026-08-02 12:32:39 +0530

    Remove IDE and build files

--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/Industrial-SCADA-Dashboard.dir/Industrial-SCADA-Dashboard_autogen/3YJK5W5UP7/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Industrial-SCADA-Dashboard.dir/Industrial-SCADA-Dashboard_autogen/3YJK5W5UP7/qrc_resources.cpp.obj: \
- C:\Dev\Industrial-SCADA-Dashboard\build\Desktop_Qt_6_11_1_MinGW_64_bit_Debug\Industrial-SCADA-Dashboard_autogen\3YJK5W5UP7\qrc_resources.cpp
--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/Industrial-SCADA-Dashboard_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Industrial-SCADA-Dashboard_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
