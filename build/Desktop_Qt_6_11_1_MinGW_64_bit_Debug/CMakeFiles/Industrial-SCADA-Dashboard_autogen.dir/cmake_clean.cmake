f4dc2a80d753a6236af549490f349c3b2182b2d7
naikpoojagetoewit <161451264+naikpoojagetoewit@users.noreply.github.com>
2026-08-02 12:36:52 +0530

    Remove old screenshot

---
+++ build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/Industrial-SCADA-Dashboard_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Industrial-SCADA-Dashboard_autogen"
+  "Industrial-SCADA-Dashboard_autogen/mocs_compilation.cpp"
+  "Industrial-SCADA-Dashboard_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Industrial-SCADA-Dashboard_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
