d45d0adf3bf61efb884a55a44c44c9536576104e
naikpoojagetoewit <161451264+naikpoojagetoewit@users.noreply.github.com>
2026-08-02 12:39:04 +0530

    Remove IDE and build files

--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Industrial-SCADA-Dashboard_FILE C:/Dev/Industrial-SCADA-Dashboard/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/Industrial-SCADA-Dashboard.exe)
-set(__QT_DEPLOY_TARGET_Industrial-SCADA-Dashboard_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Industrial-SCADA-Dashboard_RUNTIME_DLLS C:/Qt/6.11.1/mingw_64/bin/Qt6Charts.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
